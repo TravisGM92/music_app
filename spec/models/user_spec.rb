4112d0c550fd8549fc691fc4e387f92b1d93af42
Travis <mckinstrytg@gmail.com>
2023-05-23 21:43:16 +0000

    Testing pipeline

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :first_name }
     it { should have_many :addresses }
   end
+  describe 'test it' do
+    it 'works' do
+
+    end
+  end
 end
